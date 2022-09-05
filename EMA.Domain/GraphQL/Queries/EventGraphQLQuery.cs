be3a96a955e05bd448fc3f95ae9edc0636cf9839
dimainnature <dimainnature@yandex.ru>
2022-09-05 07:22:46 +0400

    feat: Add first GraphQL query.

---
+++ EMA.Domain/GraphQL/Queries/EventGraphQLQuery.cs
@@ -0,0 +1,18 @@
+﻿namespace EMA.Domain.GraphQL.Queries;
+
+public class EventGraphQLQuery
+{
+	private readonly IGenericRepository<EventEntity> _context;
+
+	public EventGraphQLQuery(
+		IGenericRepository<EventEntity> context)
+	{
+		_context = context;
+	}
+
+	[UseProjection]
+	[UseSorting]
+	[UseFiltering]
+	public IEnumerable<EventEntity> GetEventsList() =>
+		_context.GetAll();
+}
